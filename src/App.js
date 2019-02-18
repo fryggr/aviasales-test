6fcc8b5fb7591929a5e1c7afb57820b6d2169bd1
Daria Sminova <daria@realrussialimited.local>
2019-02-18 14:56:08 +0400

    add Filters block

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css'
+import './App.scss'
 
 class App extends Component {
   render() {
@@ -8,8 +8,31 @@ class App extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="col-3 Filters">
-							<div className="Filters__currency">
-								<h3 class></h3>
+							<div className="Currency">
+								<h3 className="Filters__title">ВАЛЮТА</h3>
+                <div className="Currency__radio">
+                  <input type="radio" className="Currency__input" name="currency" value="rub" id="rub" checked/>
+                	<label htmlFor="rub">rub</label>
+                  <input type="radio" className="Currency__input" name="currency" value="usd" id="usd" />
+                	<label htmlFor="usd">usd</label>
+                  <input type="radio" className="Currency__input" name="currency" value="eur" id="eur" />
+                	<label htmlFor="eur">eur</label>
+                </div>
+							</div>
+							<div className="Transfer">
+								<h3 className="Filters__title">КОЛИЧЕСТВО ПЕРЕСАДОК</h3>
+                <div className="Transfer__check">
+                  <input type="checkbox" className="Transfer__input" name="transfer" value="all-transfers" id="all-transfers" />
+                	<label htmlFor="all-transfers">Все</label>
+                  <input type="checkbox" className="Transfer__input" name="transfer" value="without-transfer" id="without-transfer" checked/>
+                	<label htmlFor="without-transfer">Без пересадок</label>
+                  <input type="checkbox" className="Transfer__input" name="transfer" value="1-transfer" id="1-transfer" checked/>
+                	<label htmlFor="1-transfer">1 пересадка</label>
+                  <input type="checkbox" className="Transfer__input" name="transfer" value="2-transfer" id="2-transfer" checked/>
+                	<label htmlFor="2-transfer">2 пересадки</label>
+                  <input type="checkbox" className="Transfer__input" name="transfer" value="3-transfer" id="3-transfer" />
+                	<label htmlFor="3-transfer">3 пересадки</label>
+                </div>
 							</div>
 						</div>
 						<div className="col-9 Tickets">
