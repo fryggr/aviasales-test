6f457edfb64c733603cd15f272b3aadaea8784b0
Daria Smirnova <fryggr@gmail.com>
2019-02-18 09:51:09 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+				<div className="container">
+					<div className="row">
+						<div className="col-3 Filters">
+							<div className="Filters__currency">
+								<h3 class></h3>
+							</div>
+						</div>
+						<div className="col-9 Tickets">
+							<div className="Tickets__item"></div>
+						</div>
+					</div>
+				</div>
       </div>
     );
   }
