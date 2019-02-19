30984dc1d612b12521e71c94b3930f9843f23e22
Daria Sminova <daria@realrussialimited.local>
2019-02-19 12:31:17 +0400

    add load tickets

--- src/components/FlightSearch/index.js
+++ src/components/FlightSearch/index.js
@@ -1,38 +1,53 @@
 import React, { Component } from 'react'
 import './index.scss'
+import tickets from '../../tickets.json'
 
 class FlightSearch extends Component {
+
+  componentDidMount() {
+      this.loadTickets()
+  }
+
+  loadTickets() {
+    console.log(tickets.tickets);
+    return tickets.tickets
+  }
+
   render() {
     return (
       <div className="col-9 FlightSearch">
-        <div className="FlightSearch__item">
-          <div className="row h-100 m-0">
-            <div className="col-4 FlightSearch__price">
-              <button className="button">Купить <br/> за 22 245 ​₽</button>
-            </div>
-            <div className="col-8 FlightSearch__info">
-              <div className="FlightSearch__time row mb-2">
-                <div className="col-4 FlightSearch__destination-time text-left">09:25</div>
-                <div className="col-4 FlightSearch__transfer">
-                  <div className="FlightSearch__transfer-title">1 ПЕРЕСАДКА</div>
-                  <div className="FlightSearch__transfer-icon mx-auto"></div>
+        {this.loadTickets().map(ticket => {
+          return (
+              <div className="FlightSearch__item">
+                <div className="row h-100 m-0">
+                  <div className="col-4 FlightSearch__price">
+                    <button className="button">Купить <br/> за {ticket.price} ​₽</button>
+                  </div>
+                  <div className="col-8 FlightSearch__info">
+                    <div className="FlightSearch__time row mb-2">
+                      <div className="col-4 FlightSearch__destination-time text-left">{ticket.arrival_time}</div>
+                      <div className="col-4 FlightSearch__transfer">
+                        <div className="FlightSearch__transfer-title">{ticket.stops} ПЕРЕСАДКА</div>
+                        <div className="FlightSearch__transfer-icon mx-auto"></div>
+                      </div>
+                      <div className="col-4 FlightSearch__destination-time text-right">{ticket.departure_time}</div>
+                    </div>
+                    <div className="FlightSearch__destination row">
+                      <div className="col-4 text-left">
+                        <div className="FlightSearch__destination-point">{ticket.origin_name}, {ticket.origin}</div>
+                        <div className="FlightSearch__destination-date">{ticket.arrival_date}</div>
+                      </div>
+                      <div className="col-4"></div>
+                    <div className="col-4 text-right">
+                        <div className="FlightSearch__destination-point">{ticket.destination_name}, {ticket.destination}</div>
+                        <div className="FlightSearch__destination-date">{ticket.departure_date}</div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-4 FlightSearch__destination-time text-right">11:45</div>
               </div>
-              <div className="FlightSearch__destination row">
-                <div className="col-4 text-left">
-                  <div className="FlightSearch__destination-point">VVO, Владивосток</div>
-                  <div className="FlightSearch__destination-date">9 окт 2018, Пт</div>
-                </div>
-                <div className="col-4"></div>
-              <div className="col-4 text-right">
-                  <div className="FlightSearch__destination-point">Тель-Авив, TLV</div>
-                  <div className="FlightSearch__destination-date">10 окт 2018, Пт</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          )
+        })}
       </div>
     )
   }
